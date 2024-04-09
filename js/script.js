69e5920bbe01a762eb5a3915a82c41a9904c9c22
MatteD04 <matteo.dantonio04@gmail.com>
2024-04-09 16:46:34 +0200

    feat: add random email with API

--- js/script.js
+++ js/script.js
@@ -4,8 +4,21 @@ const { createApp} = Vue;
 createApp ({
     data() {
         return {
+            email : []
         };
     },
     methods: {
+    },
+    mounted() {
+        //genero per 10 volte un email casuale
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                const emailGen = response.data;
+
+                //pusho l'email nell'array email
+                this.email.push(emailGen.response);
+            });
+        } 
     }
 }).mount('#app');
